3683c6a521d8bdd8d31740f092692c8c147735ae
emil7809 <emil7809@gmail.com>
2018-11-05 18:20:43 +0100

    AAARG HVAD GØR JEG

--- script.js
+++ script.js
@@ -2,14 +2,9 @@ window.addEventListener("load", sidenVises);
 
 function sidenVises() {
     console.log("siden vises");
-    hideToWin();
-}
-
-function hideToWin() {
-    console.log("hide to win");
-    document.querySelector("#towin").classList.add("hide");
     showStart();
 }
+
 
 function showStart() {
     console.log("show start");
@@ -39,39 +34,33 @@ function startGame() {
     document.querySelector("#penguin2").classList.add("drifter");
     document.querySelector("#Star1").addEventListener("click", clickStar);
     document.querySelector("#Star2").addEventListener("click", clickStar);
-    document.querySelector("#Star3").addEventListener("click", clickNoStar);
+    document.querySelector("#Star3").addEventListener("click", clickStar);
     document.querySelector("#Star4").addEventListener("click", clickStar);
     document.querySelector("#Star5").addEventListener("click", clickStar);
     document.querySelector("#Star6").addEventListener("click", clickStar);
-    document.querySelector("#Star7").addEventListener("click", clickNoStar);
+    document.querySelector("#Star7").addEventListener("click", clickStar);
     document.querySelector("#Star8").addEventListener("click", clickStar);
     document.querySelector("#Star9").addEventListener("click", clickStar);
-    document.querySelector("#Star0").addEventListener("click", clickNoStar);
+    document.querySelector("#Star0").addEventListener("click", clickStar);
 
 }
 
 let point = 0;
-let time = 70;
+let energy = 30;
 
-if (time > 70) {
-    console.log("click star");
-    time += 0;
-    console.log(time);
-}
+
 
 function clickStar() {
     console.log("click star");
-    point += 1;
-    time += 10;
-    console.log(point);
-    console.log(time);
-    this.classList.add("win");
-    this.removeEventListener("click", clickStar);
-}
+    if (this.classList.contains("sparkel")) {
+        point += 1;
+        console.log(point);
+        this.classList.add("win");
+    }
+    if (this.classList.contains("evil")) {
+        energy -= 10;
+        console.log(energy);
+        this.classList.add("fail");
+    }
 
-function clickNoStar() {
-    console.log("click no star");
-    time -= 24;
-    console.log(time);
-    this.classList.add("fail");
 }
